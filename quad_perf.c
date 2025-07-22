9be8480066272a93d00bc06d6643a95b226782db
Ana Carolina Morone <83526857+anamorone@users.noreply.github.com>
2025-07-22 09:08:20 -0300

    Create quad_perf.c
    Corrigindo exercício da P2.

---
+++ quad_perf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool quad_perf(int num){
+    int i, quad, cont;
+    for(i = 0; i < num; i++){
+        if(i * i == num){
+            cont++;
+        }
+    }
+    return cont;
+}
+
+int main()
+{
+    int num;
+    scanf("%d", &num);
+    if(quad_perf(num) == 1){
+        printf("É um quadrado perfeito");
+    }else{
+        printf("Não é um quadrado perfeito");
+    }
+    
+}
